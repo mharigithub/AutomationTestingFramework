b77bb6031fbe7a5cb1c7cd4471f66c28322b603a
haribabu.maila <haribabu.maila@gavstech.com>
2023-01-03 20:29:18 +0530

    code changes

--- src/test/java/utilities/JiraExecuteInZephyrTestCycles/GetScenarioIssueIDsUsingKeyFromJiraAPI.java
+++ src/test/java/utilities/JiraExecuteInZephyrTestCycles/GetScenarioIssueIDsUsingKeyFromJiraAPI.java
@@ -7,8 +7,12 @@ import static io.restassured.RestAssured.given;
 public class GetScenarioIssueIDsUsingKeyFromJiraAPI {
   public static void main(String[] args) {
     String issueKey = "QA-3";
+/*
+    jira api documentation link
+    https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-issues/#api-rest-api-2-issue-issueidorkey-get
+*/
     String gerURIForScenarioIssueIDsBasedOnKeyFromJira =
-            "http://eastern.atlassian.net/rest/api/2/issue" + issueKey;
+            "http://eastern.atlassian.net/rest/api/2/issue/" + issueKey;
 
     Response responseScenarioIssueIDsBasedOnKeyFromJira =
             given()
